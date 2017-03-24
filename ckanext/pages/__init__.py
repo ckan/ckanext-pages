8b984685ddbd1790d2ff41d299a8ace3b6121688
Florian Brucker <florian.brucker@it.karlsruhe.de>
2017-03-24 17:32:11 +0100

    Declare version and re-use it in setup.py
    Declares the version in `ckanext.pages.__version__` and re-uses it in
    `setup.py`.

--- ckanext/pages/__init__.py
+++ ckanext/pages/__init__.py
@@ -1,1 +1,3 @@
+# encoding: utf-8
 
+__version__ = u'0.1.0'
--- setup.py
+++ setup.py
@@ -1,37 +1,55 @@
+# encoding: utf-8
+
+import io
+import os.path
+import re
+
 from setuptools import setup, find_packages
 
-version = '0.1'
+
+# Extract version
+HERE = os.path.abspath(os.path.dirname(__file__))
+INIT_PY = os.path.join(HERE, 'ckanext', 'pages', '__init__.py')
+version = None
+with io.open(INIT_PY) as f:
+    for line in f:
+        m = re.match(r'__version__\s*=\s*u?[\'"](.*)[\'"]', line)
+        if m:
+            version = m.groups()[0]
+            break
+if version is None:
+    raise RuntimeError('Could not extract version from "{}".'.format(INIT_PY))
+
 
 setup(
-	name='ckanext-pages',
-	version=version,
-	description='Basic CMS extension for ckan',
-	long_description='',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='David Raznick',
-	author_email='david.raznick@gokfn.org',
-	url='',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext'],
-	include_package_data=True,
-	package_data={
+    name='ckanext-pages',
+    version=version,
+    description='Basic CMS extension for ckan',
+    long_description='',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='David Raznick',
+    author_email='david.raznick@gokfn.org',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext'],
+    include_package_data=True,
+    package_data={
             '': ['theme/*/*.html', 'theme/*/*/*.html', 'theme/*/*/*/*.html'],
-	},
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    },
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points=\
+    """
         [ckan.plugins]
         pages=ckanext.pages.plugin:PagesPlugin
         textboxview=ckanext.pages.plugin:TextBoxView
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
-
-	""",
+    """,
     message_extractors={
         'ckanext': [
             ('**.py', 'python', None),
