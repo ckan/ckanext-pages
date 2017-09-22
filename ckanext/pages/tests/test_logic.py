4bdb71f1fddf328706d4a54e85acbf27d04b59b5
Florian Brucker <florian.brucker@it.karlsruhe.de>
2017-09-22 14:06:33 +0200

    [#58] Add tests for page listing end points

--- ckanext/pages/tests/test_logic.py
+++ ckanext/pages/tests/test_logic.py
@@ -9,9 +9,9 @@ except ImportError:
 from ckanext.pages import db
 
 
-class TestUpdate(helpers.FunctionalTestBase):
+class TestPages(helpers.FunctionalTestBase):
     def setup(self):
-        super(TestUpdate, self).setup()
+        super(TestPages, self).setup()
         if db.pages_table is None:
             db.init_db(model)
         self.user = factories.Sysadmin()
@@ -68,3 +68,33 @@ class TestUpdate(helpers.FunctionalTestBase):
         assert_in('<h1 class="page-heading">Disallowed</h1>', response.body)
         assert_in('Test Link', response.body)
         assert_not_in('<a href="/test">Test Link</a>', response.body)
+
+    def test_pages_index(self):
+        env = {'REMOTE_USER': self.user['name'].encode('ascii')}
+        url = toolkit.url_for('pages_index')
+        response = self.app.get(url, status=200, extra_environ=env)
+        assert_in('<h2>Pages</h2>', response.body)
+        assert_in('Add page</a>', response.body)
+
+    def test_blog_index(self):
+        env = {'REMOTE_USER': self.user['name'].encode('ascii')}
+        url = toolkit.url_for('blog_index')
+        response = self.app.get(url, status=200, extra_environ=env)
+        assert_in('<h2>Blog</h2>', response.body)
+        assert_in('Add Article</a>', response.body)
+
+    def test_organization_pages_index(self):
+        env = {'REMOTE_USER': self.user['name'].encode('ascii')}
+        org = factories.Organization()
+        url = toolkit.url_for('organization_pages_index', id=org['id'])
+        response = self.app.get(url, status=200, extra_environ=env)
+        assert_in('<h2>Pages</h2>', response.body)
+        assert_in('Add page</a>', response.body)
+
+    def test_group_pages_index(self):
+        env = {'REMOTE_USER': self.user['name'].encode('ascii')}
+        group = factories.Group()
+        url = toolkit.url_for('group_pages_index', id=group['id'])
+        response = self.app.get(url, status=200, extra_environ=env)
+        assert_in('<h2>Pages</h2>', response.body)
+        assert_in('Add page</a>', response.body)
