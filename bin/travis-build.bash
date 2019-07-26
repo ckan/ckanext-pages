30949649d4b4f497709c40502160ea3c78091f70
amercader <amercadero@gmail.com>
2019-07-26 16:45:15 +0200

    Use single core Solr

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -31,9 +31,10 @@ sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 
 echo "SOLR config..."
-echo "Setting up Solr..."
 printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+
+sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
 sudo service jetty restart
 
 echo "Initialising the database..."
