b5887fef01580c5d32dbaa1ca0223c07fb640b43
jyri <soppela.jyri@gmail.com>
2019-11-19 11:23:01 +0200

    refactor Travis setup

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,7 +5,7 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty libcommons-fileupload-java
+sudo apt-get install solr-jetty
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
@@ -18,35 +18,49 @@ else
     git checkout $CKAN_TAG
     echo "CKAN version: ${CKAN_TAG#ckan-}"
 fi
-# Unpin CKAN's psycopg2 dependency get an important bugfix
-# https://stackoverflow.com/questions/47044854/error-installing-psycopg2-2-6-2
-sed -i '/psycopg2/c\psycopg2' requirements.txt
-python setup.py develop
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
+
+# install the recommended version of setuptools
+if [ -f requirement-setuptools.txt ]
+then
+    echo "Sudo Updating setuptools..."
+    pip install --user -r requirement-setuptools.txt
+fi
+
+if [ $CKANVERSION == '2.7' ]
+then
+    echo "Installing setuptools"
+    pip install --user setuptools==39.0.1
+fi
+
+sudo python setup.py develop
+pip install --user -r requirements.txt
+pip install --user -r dev-requirements.txt
+pip install --user flake8
 cd -
 
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 
-echo "SOLR config..."
+echo "Setting up Solr..."
 # Solr is multicore for tests on ckan master, but it's easier to run tests on
 # Travis single-core. See https://github.com/ckan/ckan/issues/2972
 sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 
 echo "Initialising the database..."
 cd ckan
 paster db init -c test-core.ini
 cd -
 
-echo "Installing ckanext-pages and its requirements..."
-python setup.py develop
-pip install -r dev-requirements.txt
+echo "Installing ckanext-restricted and its requirements..."
+sudo python setup.py develop
+pip install --user -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
 mv test.ini subdir
 
 echo "travis-build.bash is done."
-
--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -4,12 +4,21 @@ echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" |
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
+set -ex
+
+flake8 --version
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,{{ project }}
+
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
           --with-coverage \
-          --cover-package=ckanext.pages \
+          --cover-package=ckanext.restricted \
           --cover-inclusive \
           --cover-erase \
-          --cover-tests
+          --cover-tests \
+          ckanext/restricted
 
+# strict linting
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan,{{ project }}
