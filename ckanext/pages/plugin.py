17467632f31dbfafdbca81393dbb2f9c56040616
patricksikalinda <sikalindapatrick@gmail.com>
2020-06-23 14:24:19 +0200

    ckan.root_path fix

--- ckanext/pages/plugin.py
+++ ckanext/pages/plugin.py
@@ -59,7 +59,7 @@ def build_pages_nav_main(*args):
         type_ = 'blog' if page['page_type'] == 'blog' else 'pages'
         name = urllib.quote(page['name'].encode('utf-8')).decode('utf-8')
         title = cgi.escape(page['title'])
-        link = h.literal(u'<a href="/{}/{}">{}</a>'.format(type_, name, title))
+        link = h.literal(u'<a href="{}/{}/{}">{}</a>'.format(root_path, type_, name, title))
         if page['name'] == page_name:
             li = h.literal('<li class="active">') + link + h.literal('</li>')
         else:
