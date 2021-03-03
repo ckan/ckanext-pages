3edb696bbb44bc4249d82f98912edb0517c665a6
amercader <amercadero@gmail.com>
2021-03-03 12:22:02 +0100

    Fix tests on 2.9-py2

--- ckanext/pages/tests/fixtures.py
+++ ckanext/pages/tests/fixtures.py
@@ -9,3 +9,10 @@ from ckanext.pages import db
 def pages_setup():
     if db.pages_table is None:
         db.init_db(model)
+
+
+@pytest.fixture
+def clean_pages():
+    if db.pages_table is not None:
+        model.Session.query(db.Page).delete()
+        model.Session.commit()
--- ckanext/pages/tests/test_logic.py
+++ ckanext/pages/tests/test_logic.py
@@ -8,18 +8,23 @@ from ckantoolkit.tests import factories, helpers
 
 from ckanext.pages.logic import schema
 
+ckan_29_or_higher = toolkit.check_ckan_version(u'2.9')
 
-@pytest.mark.usefixtures("clean_db", "pages_setup")
+
+@pytest.mark.usefixtures("clean_db", "clean_pages", "pages_setup")
 class TestPages():
 
     def test_create_page(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_page'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_page'),
+            url=toolkit.url_for('pages_edit', page=page),
             params={
                 'title': 'Page Title',
                 'name': 'page_name',
+                'private': False,
             },
             extra_environ=env,
         )
@@ -29,16 +34,20 @@ class TestPages():
     def test_rendering_with_html_allowed(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_html_page'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_html_page'),
+            url=toolkit.url_for('pages_edit', page=page),
             params={
                 'title': 'Allowed',
                 'name': 'page_html_allowed',
                 'content': '<a href="/test">Test Link</a>',
+                'private': False,
             },
             extra_environ=env,
         )
-        response = response.follow(extra_environ=env)
+        if not ckan_29_or_higher:
+            response = response.follow(extra_environ=env)
         assert '<h1 class="page-heading">Allowed</h1>' in response.body
         if toolkit.check_ckan_version(min_version='2.3'):
             assert '<a href="/test">Test Link</a>' in response.body
@@ -49,16 +58,21 @@ class TestPages():
     def test_rendering_with_html_disallowed(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_html_page'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_html_page'),
+            url=toolkit.url_for('pages_edit', page='test_html_page'),
             params={
                 'title': 'Disallowed',
                 'name': 'page_html_disallowed',
                 'content': '<a href="/test">Test Link</a>',
+                'private': False,
             },
             extra_environ=env,
         )
-        response = response.follow(extra_environ=env)
+
+        if not ckan_29_or_higher:
+            response = response.follow(extra_environ=env)
         assert '<h1 class="page-heading">Disallowed</h1>' in response.body
         assert 'Test Link' in response.body
         assert '<a href="/test">Test Link</a>' not in response.body
@@ -67,32 +81,42 @@ class TestPages():
     def test_rendering_no_p_tags_added_with_html_disallowed(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_html_page_p'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_html_page_p'),
+            url=toolkit.url_for('pages_edit', page=page),
             params={
                 'title': 'Disallowed',
                 'name': 'page_html_disallowed_p',
                 'content': 'Hi there **you**',
+                'private': False,
             },
             extra_environ=env,
         )
-        response = response.follow(extra_environ=env)
+
+        if not ckan_29_or_higher:
+            response = response.follow(extra_environ=env)
         assert '<p>Hi there <strong>you</strong></p>' in response.body
 
     @pytest.mark.ckan_config('ckanext.pages.allow_html', True)
     def test_rendering_no_div_tags_added_with_html_allowed(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_html_page_div'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_html_page_div'),
+            url=toolkit.url_for('pages_edit', page=page),
             params={
                 'title': 'Disallowed',
                 'name': 'page_html_allowed_div',
                 'content': '<p>Hi there</p>',
+                'private': False,
             },
             extra_environ=env,
         )
-        response = response.follow(extra_environ=env)
+
+        if not ckan_29_or_higher:
+            response = response.follow(extra_environ=env)
         assert '<p>Hi there</p>' in response.body
         assert '<div><p>Hi there</p></div>' not in response.body
 
@@ -107,7 +131,8 @@ class TestPages():
     def test_blog_index(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
-        url = toolkit.url_for('blog_index')
+        endpoint = 'pages.blog_index' if ckan_29_or_higher else 'blog_index'
+        url = toolkit.url_for(endpoint)
         response = app.get(url, status=200, extra_environ=env)
         assert '<h2>Blog</h2>' in response.body
         assert 'Add Article</a>' in response.body
@@ -116,7 +141,9 @@ class TestPages():
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
         org = factories.Organization()
-        url = toolkit.url_for('organization_pages_index', id=org['id'])
+
+        endpoint = 'pages.organization_pages_index' if ckan_29_or_higher else 'organization_pages_index'
+        url = toolkit.url_for(endpoint, id=org['id'])
         response = app.get(url, status=200, extra_environ=env)
         assert '<h2>Pages</h2>' in response.body
         assert 'Add page</a>' in response.body
@@ -125,7 +152,8 @@ class TestPages():
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
         group = factories.Group()
-        url = toolkit.url_for('group_pages_index', id=group['id'])
+        endpoint = 'pages.group_pages_index' if ckan_29_or_higher else 'group_pages_index'
+        url = toolkit.url_for(endpoint, id=group['id'])
         response = app.get(url, status=200, extra_environ=env)
         assert '<h2>Pages</h2>' in response.body
         assert 'Add page</a>' in response.body
@@ -133,8 +161,10 @@ class TestPages():
     def test_unicode(self, app):
         user = factories.Sysadmin()
         env = {'REMOTE_USER': user['name'].encode('ascii')}
+        page = 'test_html_page_div'
+        page = '/' + page if not ckan_29_or_higher else page
         response = app.post(
-            url=toolkit.url_for('pages_edit', page='/test_unicode_page'),
+            url=toolkit.url_for('pages_edit', page=page),
             params={
                 'title': u'Tïtlé'.encode('utf-8'),
                 'name': 'page_unicode',
@@ -144,14 +174,22 @@ class TestPages():
             },
             extra_environ=env,
         )
-        response = response.follow(extra_environ=env)
-        assert u'<title>Tïtlé - CKAN</title>' in response.unicode_body
-        assert u'<a href="/pages/page_unicode">Tïtlé</a>' in response.unicode_body
-        assert u'<h1 class="page-heading">Tïtlé</h1>' in response.unicode_body
-        if toolkit.check_ckan_version(min_version='2.8.0'):
-            assert u'<p>&#199;&#246;&#241;t&#233;&#241;t</p>' in response.unicode_body
+
+        if not ckan_29_or_higher:
+            response = response.follow(extra_environ=env)
+        if toolkit.check_ckan_version(min_version='2.9.0'):
+            assert u'<p>&#199;&#246;&#241;t&#233;&#241;t</p>' in response.get_data(as_text=True)
+            assert u'<title>Tïtlé - CKAN</title>' in response.get_data(as_text=True)
+            assert u'<a href="/pages/page_unicode">Tïtlé</a>' in response.get_data(as_text=True)
+            assert u'<h1 class="page-heading">Tïtlé</h1>' in response.get_data(as_text=True)
         else:
-            assert u'<p>Çöñtéñt</p>' in response.unicode_body
+            assert u'<title>Tïtlé - CKAN</title>' in response.unicode_body
+            assert u'<a href="/pages/page_unicode">Tïtlé</a>' in response.unicode_body
+            assert u'<h1 class="page-heading">Tïtlé</h1>' in response.unicode_body
+            if toolkit.check_ckan_version(min_version='2.8.0'):
+                assert u'<p>&#199;&#246;&#241;t&#233;&#241;t</p>' in response.unicode_body
+            else:
+                assert u'<p>Çöñtéñt</p>' in response.unicode_body
 
     def test_pages_saves_custom_schema_fields(self, app):
         user = factories.Sysadmin()
