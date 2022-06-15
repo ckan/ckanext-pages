531d2e1dbb40cdf29065dc06c430a404ef84438d
pdelboca <patriciodelboca@gmail.com>
2022-06-15 21:29:30 +0200

    Add some action tests

---
+++ ckanext/pages/tests/test_action.py
@@ -0,0 +1,57 @@
+import pytest
+
+from ckan.plugins import toolkit
+from ckan.tests import factories, helpers
+
+@pytest.mark.usefixtures("clean_db", "pages_setup")
+class TestPagesActions():
+
+    def test_pages_create_action(self, app):
+        user = factories.User()
+        helpers.call_action(
+            'ckanext_pages_update',
+            {'user': user['name']},
+            name='page_name', title='New Page', content='This is a test content'
+        )
+
+        page = helpers.call_action(
+            'ckanext_pages_show',
+            {},
+            page='page_name')
+
+        assert page['name'] == 'page_name'
+        assert page['title'] == 'New Page'
+        assert page['content'] == 'This is a test content'
+
+    def test_pages_update_action(self, app):
+        user = factories.User()
+        helpers.call_action(
+            'ckanext_pages_update',
+            {'user': user['name']},
+            name='page_name', title='New Page', content='This is a test content'
+        )
+
+        page = helpers.call_action(
+            'ckanext_pages_show',
+            {},
+            page='page_name')
+
+        assert page['name'] == 'page_name'
+        assert page['title'] == 'New Page'
+        assert page['content'] == 'This is a test content'
+
+        # sending the parameter page is mandatory for the validator to pass.
+        helpers.call_action(
+            'ckanext_pages_update',
+            {'user': user['name']},
+            name='page_name', title='New Page Updated', content='This is a test content updated', page='page_name'
+        )
+
+        page = helpers.call_action(
+            'ckanext_pages_show',
+            {},
+            page='page_name')
+
+        assert page['name'] == 'page_name'
+        assert page['title'] == 'New Page Updated'
+        assert page['content'] == 'This is a test content updated'
